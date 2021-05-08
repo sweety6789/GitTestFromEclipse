33059fd64c3fbadc14998738d4d8436479bb55d6
Aishwarya M <aishu.devang5@gmail.com>
2021-05-08 08:10:50 +0530

    First Commit....

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		
+		boolean practise_daily = true;
+		
+		
+		
+		if(practise_daily)
+		{
+			System.out.println("Success");
+		}
+		else
+		{
+			System.out.println("No Success");
+		}
+	}
+
+}
